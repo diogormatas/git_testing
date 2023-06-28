ddafb73c32afe473b3fbe79555d7fb6116b7c457
diogormatas <diogormatas@gmail.com>
2023-06-28 15:49:49 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,21 @@
 import requests
+import argparse
 
-def main():
+
+def deploy_to_environment(connection_string):
+    # Lógica para implantação no ambiente com base na conexão fornecida
+    print(f"Deploying to environment with connection string: {connection_string}")
+    # Restante da lógica de implantação
     print("Hello, World!")
     response = requests.get("https://www.example.com")
     print("Response status code:", response.status_code)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--connection-string", required=True, help="Connection string for the environment")
+    args = parser.parse_args()
+
+    deploy_to_environment(args.connection_string)
 
 if __name__ == "__main__":
     main()
