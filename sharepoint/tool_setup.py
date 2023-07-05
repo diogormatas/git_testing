4e7fdaa076b3c3ae08686165160c6a1d2a1aa083
diogormatas <diogormatas@gmail.com>
2023-07-05 15:27:09 +0100

    Add files via upload

---
+++ sharepoint/tool_setup.py
@@ -0,0 +1,31 @@
+import requests
+import argparse
+
+# corri em development porque fizeram o pull-request (por aprovar) de development->uat
+# corri em uat porque fizeram o pull-request (por aprovar) de uat->main
+# corri em prod porque aprovaram o pull-request de uat->main
+# teste final 2023 06 28
+# teste final 2023 06 28 v2
+# teste final 2023 06 28 v3
+# teste final 2023 06 28 v4
+# teste para testar so com MERGEs e aproximar do GitFlow
+
+def deploy_to_environment(connection_string):
+    # Lógica para implantação no ambiente com base na conexão fornecida
+    print("-------- entrei no codigo da App Python -----------")
+    print("teste para testar so com MERGEs e aproximar do GitFlow")
+    print(f"Deploying to environment with connection string: {connection_string}")
+    # Restante da lógica de implantação
+    print("Hello, World from development branch -> merged to main!")
+    response = requests.get("https://www.example.com")
+    print("Response status code:", response.status_code)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--connection-string", required=True, help="Connection string for the environment")
+    args = parser.parse_args()
+
+    deploy_to_environment(args.connection_string)
+
+if __name__ == "__main__":
+    main()
---
+++ sharepoint/tool_test.py
@@ -0,0 +1,16 @@
+import pytest
+from tool_setup import deploy_to_environment
+
+# comentario dummy
+def test_deploy_to_environment(capsys):
+    connection_string = "test_connection_string"
+
+    deploy_to_environment(connection_string)
+
+    captured = capsys.readouterr()
+    assert "Deploying to environment with connection string: test_connection_string" in captured.out
+    assert "Hello, World from development branch -> merged to main!" in captured.out
+
+
+if __name__ == "__main__":
+    pytest.main()
